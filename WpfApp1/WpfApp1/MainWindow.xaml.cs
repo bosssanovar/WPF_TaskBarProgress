12c45b4c6058cf409f12702e045689585821b217
kei ito <ito.kei@jvckenwood.com>
2024-03-22 13:39:42 +0900

    処理実装

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -17,22 +17,51 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         bool isProcessing = false;
+        bool canceler = false;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private async void button_Click(object sender, RoutedEventArgs e)
         {
             if (!isProcessing)
             {
+                isProcessing = true;
                 button.Content = "キャンセル";
+
+                await Do();
+
+                isProcessing = false;
+                progressBar.Value = 0;
+                button.Content = "実行";
             }
             else
             {
+                canceler = true;
                 button.Content = "実行";
             }
+        }
+
+        private async Task Do()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                if (canceler)
+                {
+                    progressBar.Value = 0;
+                    isProcessing = false;
+                    canceler = false;
+                    MessageBox.Show("キャンセル");
+                    return;
+                }
+
+                await Task.Delay(1000);
+                progressBar.Value = i+1;
+            }
+
+            MessageBox.Show("処理完了");
         }
     }
 }
