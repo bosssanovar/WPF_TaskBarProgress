b000dd1814c640cdc0243bac3759c3aa8c8483cd
kei ito <ito.kei@jvckenwood.com>
2024-03-22 13:28:24 +0900

    first commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isProcessing = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isProcessing)
+            {
+                button.Content = "キャンセル";
+            }
+            else
+            {
+                button.Content = "実行";
+            }
+        }
+    }
+}
